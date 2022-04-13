f8dd5bf9888f15ee8bff8a3a68ec89ccd3d88a61
Ade <shennaike2386@hotmail.com>
2022-04-13 01:11:50 -0400

    find by comment  endpoint works

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -75,5 +75,16 @@ const express = require('express')
     })
  })
 
- router.get('/:id/comments ', (req, res) => {})
+ router.get('/:id/comments', (req, res) => {
+    Post.findPostComments(req.params.id)
+    .then(post =>{
+        if(!post)
+        {res.status(404).json({ message: "The post with the specified ID does not exist" })}
+        else{res.status(200).json(post)}
+    })
+    .catch(err =>{
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+    })
+ })
+
  module.exports = router
