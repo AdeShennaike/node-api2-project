233fa8442294f3ed507b197f8d5ac1a025becb18
Ade <shennaike2386@hotmail.com>
2022-04-13 00:36:36 -0400

    getby id  endpoint works

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,7 +16,18 @@ const express = require('express')
         })
  })
 
- router.get('/:id ', (req, res) => {})
+ router.get('/:id', (req, res) => {
+     Post.findById(req.params.id)
+        .then(post =>{
+            if(!post)
+            {res.status(404).json({ message: "The post with the specified ID does not exist" })}
+            else{res.status(200).json(post)}
+        })
+        .catch(err =>{
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+        })
+ })
+
  router.post('/', (req, res) => {})
  router.put('/:id', (req, res) => {})
  router.delete('/:id ', (req, res) => {})
